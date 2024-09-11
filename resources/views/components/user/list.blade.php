835bf8a5ac2ee93267867d1700de406f67db5c6e
tazima-seiya <tazima@chubu-fukushi.org>
2024-09-11 13:05:02 +0900

    ユーザー情報編集画面への導線を追加

--- resources/views/components/user/list.blade.php
+++ resources/views/components/user/list.blade.php
@@ -3,24 +3,33 @@
         @foreach ($users as $user)
             <li>
                 <div>
-                    {{ $user->name }}
+                    {{ $user->name . " | " . $user->email }}
                 </div>
                 <div>
-                    {{ $user->email }}
+                    <a href="{{ route('user.update.index', ['userId' => $user->id]) }}">
+                        編集
+                    </a>
                 </div>
                 <div class="bg-white rounded-md shadow-lg mt-5 mb-5">
                     @php
                         $tweets = $user->tweets()->get();
                     @endphp
-                    <ul class="list-disc">
-                    @foreach ($tweets as $tweet)
-                        <li  class="border-b last:border-b-0 border-gray-200 p-4 flex items-start justify-between">
-                            <div class="text-gray-600">
-                                {!! nl2br(e($tweet->content)) !!}
-                            </div>
-                        </li>
-                    @endforeach
-                    </ul>
+                    <details>
+                        <summary>つぶやき一覧</summary>
+                        @if (count($tweets) === 0)
+                            <p class="text-red-400">まだつぶやいていません</p>
+                        @else
+                        <ul>
+                        @foreach ($tweets as $tweet)
+                            <li  class="border-b last:border-b-0 border-gray-200 p-4 flex items-start justify-between">
+                                <div class="text-gray-600">
+                                    {!! nl2br(e($tweet->content)) !!}
+                                </div>
+                            </li>
+                        @endforeach
+                        </ul>
+                        @endif
+                    </details>
                 </div>
             </li>
         @endforeach
