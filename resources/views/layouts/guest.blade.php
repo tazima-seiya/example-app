c810da20cadaf5e270d658b990bc61e967afd646
tazima-seiya <tazima@chubu-fukushi.org>
2024-10-04 10:06:50 +0900

    見出し追加

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,10 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-slate-300 shadow-md overflow-hidden sm:rounded-lg">
+                <h1 class="text-center text-gray-700 text-2xl font-semibold mb-1">
+                    {{ $header ?? "" }}
+                </h1>
                 {{ $slot }}
             </div>
         </div>
