adfd48965810bf84420d12bf9af0d2835a992a91
tazima-seiya <tazima@chubu-fukushi.org>
2024-10-15 11:20:50 +0900

    入力欄の文字数をカウントする機能を追加

--- resources/views/components/tweet/list.blade.php
+++ resources/views/components/tweet/list.blade.php
@@ -13,6 +13,7 @@
             <div>
                 <span class="inline-block rounded-full text-gray-600 bg-emerald-100 px-2 py-1 text-xs mb-2">
                     <div class="flex">
+                        {{-- {{ $tweet->id }}&nbsp;|&nbsp; --}}
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                           </svg>&nbsp;{{ $tweet->user->name }}
