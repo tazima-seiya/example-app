c51071bc55efea776e45c4701071457c32551581
tazima-seiya <tazima@chubu-fukushi.org>
2024-10-23 10:36:54 +0900

    FontAwesomeのバージョン、ウェイトを変更

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0,
         maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
     @vite('resources/css/app.css')
     <title>{{ $title ?? 'つぶやきアプリ' }}</title>
     @stack('css')
